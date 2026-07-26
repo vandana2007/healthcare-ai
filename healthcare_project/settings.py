4fb9194aae95c7f22956e80c7e3b3b0d0fe0d17d
vandana2007 <maddipatlavandana@gmail.com>
2026-07-26 15:51:06 +0530

    Add STATIC_ROOT for production static file collection

--- healthcare_project/settings.py
+++ healthcare_project/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # --------------------------------------------------
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
